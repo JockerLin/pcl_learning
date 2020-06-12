6c846ace0c502861a8cb15c642e5b73bd9b6c8d0
JockerLin <lczq@vip.qq.com>
2020-06-12 17:45:06 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/pcl_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_learning.dir/main.cpp.o"
+  "pcl_learning"
+  "pcl_learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
